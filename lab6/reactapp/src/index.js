afdcdfea0d3a2acaadca612d4c8250205889e855
mprzybolewski <mprzybolewski@gmail.com>
2019-04-14 09:26:42 +0200

    lab6 solutions

---
+++ lab6/reactapp/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+function Play() {
+    return <h2>Bum bum bum</h2>;
+}
+
+ReactDOM.render(<Play />, document.getElementById('root'))
